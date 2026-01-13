1a58adf049a45701cd75565caf9bea97cfa559b7
mnoble01 <mnoble@salsify.com>
2026-01-13 12:28:56 -0600

    update rslib storybook

--- rslib/rslib-component-library/stories/Button.stories.ts
+++ rslib/rslib-component-library/stories/Button.stories.ts
@@ -1,10 +1,10 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { fn } from 'storybook/test';
-import { Button } from '../src/Button';
+import Button from '../src/components/Button';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
-  title: 'Example/Button',
+  title: 'Actions/Button',
   component: Button,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
@@ -14,10 +14,10 @@ const meta = {
   tags: ['autodocs'],
   // More on argTypes: https://storybook.js.org/docs/api/argtypes
   argTypes: {
-    backgroundColor: { control: 'color' },
+    // backgroundColor: { control: 'color' },
   },
   // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
-  args: { onClick: fn() },
+  // args: { onClick: fn() },
 } satisfies Meta<typeof Button>;
 
 export default meta;
@@ -26,27 +26,24 @@ type Story = StoryObj<typeof meta>;
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Primary: Story = {
   args: {
-    primary: true,
-    label: 'Button',
+    type: 'primary'
   },
 };
 
 export const Secondary: Story = {
   args: {
-    label: 'Button',
+    type: 'secondary'
   },
 };
 
-export const Large: Story = {
+export const Tertiary: Story = {
   args: {
-    size: 'large',
-    label: 'Button',
+    type: 'tertiary'
   },
 };
 
-export const Small: Story = {
+export const Destructive: Story = {
   args: {
-    size: 'small',
-    label: 'Button',
+    type: 'destructive'
   },
 };
